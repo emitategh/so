8d46e2fdd9d8b5c304d99b4804083701d38a9432
Fercho <Fercho@localhost.localdomain>
2016-10-11 18:27:16 -0300

    Cocineros

--- Emi/CocinerosPosta.c
+++ Emi/CocinerosPosta.c
@@ -2,61 +2,32 @@
 #include<pthread.h>
 #include <semaphore.h>
 
-sem_t scocinero,scocina,sfuente,scerrado,scomensal,scomer;
+sem_t scocinero,scocina,sfuente,scomensal,scomer;
 
 void* cocineroA(){
-	int i=0;
-	while(i!=5){
+	while(1){
 		
 		sem_wait(&scocinero);
 		sem_wait(&scocina);
-		int close=sem_trywait(&scerrado);
-		if(close==0){
-			sem_post(&scerrado);
-			printf("Estoy cerrando A\n");
-			pthread_exit(0);
-		}
-		else{
-			for(int j=0;j<100;j++){
-				sem_post(&sfuente);
-				printf("Cocinando: A\n");
-			};
-			sem_post(&scomensal);
-			sem_post(&scomer);
+		for(int j=0;j<100;j++){
+			sem_post(&sfuente);
+			printf("Cocinando: A\n");
 		};
-		i++;
-		if(i==5){
-			printf("VOY A CERRAR A\n");
-			sem_post(&scerrado);
-		};
+		sem_post(&scomensal);
+		sem_post(&scomer);
 	};
 }
 
 void* cocineroB(){
-	int i=0;
-	while(i!=5){
+	while(1){
 		sem_wait(&scocinero);
 		sem_wait(&scocina);
-		int close=sem_trywait(&scerrado);
-		if(close==0){
-			sem_post(&scerrado);
-			printf("Estoy cerrando B\n");
-			pthread_exit(0);
-		}
-		else{
-			for(int j=0;j<100;j++){
-				sem_post(&sfuente);
-				printf("Cocinando: B\n");
-			};
-			sem_post(&scomensal);
-			sem_post(&scomer);
+		for(int j=0;j<100;j++){
+			sem_post(&sfuente);
+			printf("Cocinando: B\n");
 		};
-		i++;
-		if(i==5){
-			printf("VOY A CERRAR B\n");
-			sem_post(&scerrado);
-			
-		};
+		sem_post(&scomensal);
+		sem_post(&scomer);
 	};
 }
 
@@ -64,37 +35,24 @@ void* comensalA(){
 	while(1){
 		sem_wait(&scomensal);
 		sem_wait(&scomer);
-		int close=sem_trywait(&scerrado);
-		if(close==0){
-			sem_post(&scerrado);
-			int porciones=sem_trywait(&sfuente);
-			if(porciones==0){
-				printf("Juan: Me estoy sirviendo\n");
-				sem_post(&scomensal);
-				sem_post(&scomer);				
-				printf("Juan: Comiendo\n");
+		int porciones=sem_trywait(&sfuente);
+		if(porciones==0){
+			printf("Juan: Me estoy sirviendo\n");
+			sem_post(&scomensal);
+			sem_post(&scomer);
+
+			for(int j=0;j<92013;j++){
+
 			};
-			printf("Termine de comer Juan\n");
-			sem_post(&scocinero);
-			sem_post(&scocina);
-			pthread_exit(0);
+			printf("Juan: Comiendo\n");
+			
 		}
 		else{
-			int porciones=sem_trywait(&sfuente);
-			if(porciones==0){
-				printf("Juan: Me estoy sirviendo\n");
-				sem_post(&scomensal);
-				sem_post(&scomer);
-				printf("Juan: Comiendo\n");
 				
-			}
-			else{
-				
-				printf("Juan despierta a los cocineros\n");
-				sem_post(&scocinero);
-				sem_post(&scocinero);
-				sem_post(&scocina);
-			};
+			printf("Juan despierta a los cocineros\n");
+			sem_post(&scocinero);
+			sem_post(&scocinero);
+			sem_post(&scocina);
 		};
 	};
 }
@@ -102,34 +60,24 @@ void* comensalB(){
 	while(1){
 		sem_wait(&scomensal);
 		sem_wait(&scomer);
-		int close=sem_trywait(&scerrado);
-		if(close==0){
-			sem_post(&scerrado);
-			int porciones=sem_trywait(&sfuente);
-			if(porciones==0){
-				printf("Comiendo Pedro\n");
-				sem_post(&scomensal);
-				sem_post(&scomer);
+		int porciones=sem_trywait(&sfuente);
+		if(porciones==0){
+			printf("Roberto: Me estoy sirviendo\n");
+			sem_post(&scomensal);
+			sem_post(&scomer);
+
+			for(int j=0;j<92013;j++){
+
 			};
-			printf("Termine de comer Pedro\n");
-			sem_post(&scocinero);
-			sem_post(&scocina);
-			pthread_exit(0);
+			printf("Roberto: Comiendo\n");
+			
 		}
 		else{
-			int porciones=sem_trywait(&sfuente);
-			if(porciones==0){
-				printf("Pedro: Me estoy sirviendo\n");
-				sem_post(&scomensal);
-				sem_post(&scomer);
-				printf("Pedro: Comiendo\n");
-			}
-			else{
-				printf("Pedro despierta a los cocineros\n");
-				sem_post(&scocinero);
-				sem_post(&scocinero);
-				sem_post(&scocina);
-			};
+				
+			printf("Roberto despierta a los cocineros\n");
+			sem_post(&scocinero);
+			sem_post(&scocinero);
+			sem_post(&scocina);
 		};
 	};
 }
@@ -137,40 +85,130 @@ void* comensalC(){
 	while(1){
 		sem_wait(&scomensal);
 		sem_wait(&scomer);
-		int close=sem_trywait(&scerrado);
-		if(close==0){
-			sem_post(&scerrado);
-			int porciones=sem_trywait(&sfuente);
-			if(porciones==0){
-				printf("Gaston: Me estoy sirviendo\n");
-				sem_post(&scomensal);
-				sem_post(&scomer);
-				printf("Gaston: Estoy comiendo\n");
+		int porciones=sem_trywait(&sfuente);
+		if(porciones==0){
+			printf("Gaston: Me estoy sirviendo\n");
+			sem_post(&scomensal);
+			sem_post(&scomer);
+
+			for(int j=0;j<92013;j++){
+
 			};
-			printf("Termine de comer Gaston\n");
-			sem_post(&scocinero);
-			sem_post(&scocina);
-			pthread_exit(0);
+			printf("Gaston: Comiendo\n");
+			
 		}
 		else{
-			int porciones=sem_trywait(&sfuente);
-			if(porciones==0){
-				printf("Gaston: Me estoy sirviendo\n");
-				sem_post(&scomensal);
-				sem_post(&scomer); //HABILITA LA FUENTE PARA COMER
-				printf("Gaston: Estoy comiendo\n");
-			}
-			else{
-				printf("Gaston despierta a los cocineros\n");
-				sem_post(&scocinero);
-				sem_post(&scocinero);
-				sem_post(&scocina);
+				
+			printf("Gaston despierta a los cocineros\n");
+			sem_post(&scocinero);
+			sem_post(&scocinero);
+			sem_post(&scocina);
+		};
+	};
+}
+void* comensalD(){
+	while(1){
+		sem_wait(&scomensal);
+		sem_wait(&scomer);
+		int porciones=sem_trywait(&sfuente);
+		if(porciones==0){
+			printf("Cecilia: Me estoy sirviendo\n");
+			sem_post(&scomensal);
+			sem_post(&scomer);
+
+			for(int j=0;j<92013;j++){
+
 			};
+			printf("Cecilia: Comiendo\n");
+			
+		}
+		else{
+				
+			printf("Cecilia despierta a los cocineros\n");
+			sem_post(&scocinero);
+			sem_post(&scocinero);
+			sem_post(&scocina);
+		};
+	};
+}
+void* comensalE(){
+	while(1){
+		sem_wait(&scomensal);
+		sem_wait(&scomer);
+		int porciones=sem_trywait(&sfuente);
+		if(porciones==0){
+			printf("Wilson: Me estoy sirviendo\n");
+			sem_post(&scomensal);
+			sem_post(&scomer);
+
+			for(int j=0;j<92013;j++){
+
+			};
+			printf("Wilson: Comiendo\n");
+			
+		}
+		else{
+				
+			printf("Wilson despierta a los cocineros\n");
+			sem_post(&scocinero);
+			sem_post(&scocinero);
+			sem_post(&scocina);
+		};
+	};
+}
+void* comensalF(){
+	while(1){
+		sem_wait(&scomensal);
+		sem_wait(&scomer);
+		int porciones=sem_trywait(&sfuente);
+		if(porciones==0){
+			printf("Cristian: Me estoy sirviendo\n");
+			sem_post(&scomensal);
+			sem_post(&scomer);
+			for(int j=0;j<92013;j++){
+
+			};
+			printf("Cristian: Comiendo\n");
+			
+		}
+		else{
+				
+			printf("Cristian despierta a los cocineros\n");
+			sem_post(&scocinero);
+			sem_post(&scocinero);
+			sem_post(&scocina);
+		};
+	};
+}
+void* comensalG(){
+	while(1){
+		sem_wait(&scomensal);
+		sem_wait(&scomer);
+		int porciones=sem_trywait(&sfuente);
+		if(porciones==0){
+			printf("Motorola: Me estoy sirviendo\n");
+			sem_post(&scomensal);
+			sem_post(&scomer);
+
+			for(int j=0;j<92013;j++){
+				//Esto es para dar mas tiempo a que cualquier comensal vuelva a 
+				// servirse y hacer mas visible el comportamiento de
+				// que solo uno se puede servir pero varios comer al mismo tiempo
+			};
+			printf("Motorola: Comiendo\n");
+			
+		}
+		else{
+				
+			printf("Motorola despierta a los cocineros\n");
+			sem_post(&scocinero);
+			sem_post(&scocinero);
+			sem_post(&scocina);
 		};
 	};
 }
 void main(){
-	pthread_t h1,h2,h3,h4,h5;
+	pthread_t h1,h2,h3,h4,h5,h6,h7,h8,h9;
 	
 	/*Creo los semaforos*/
 	sem_init(&scocinero,0,1);
@@ -184,7 +222,13 @@ void main(){
 	pthread_create (&h2,NULL,(void*) cocineroB,NULL);
 	pthread_create (&h3,NULL,(void*) comensalA,NULL);
 	pthread_create (&h4,NULL,(void*) comensalB,NULL);
-	pthread_create (&h5,NULL,(void*) comensalC,NULL);	
+	pthread_create (&h5,NULL,(void*) comensalC,NULL);
+
+
+	pthread_create (&h6,NULL,(void*) comensalD,NULL);
+	pthread_create (&h7,NULL,(void*) comensalE,NULL);
+	pthread_create (&h8,NULL,(void*) comensalF,NULL);
+	pthread_create (&h9,NULL,(void*) comensalG,NULL);	
 	
 	/*Cierro los hilos*/ 
 	pthread_join(h1,NULL);
