c1a6d0a37ef4649bf72df52b85602df1e17c8b66
Fercho <Fercho@localhost.localdomain>
2016-11-20 21:31:01 -0300

    kaka

--- Proyecto2/ejercicioMejorLugar.c
+++ Proyecto2/ejercicioMejorLugar.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 struct celda{
 	int estado;
 	int size;
 	int ID;
-	char pro;
+	int pro;
 	struct celda* siguiente;
 	struct celda* anterior;
 	
@@ -16,6 +17,14 @@ struct celda{
 #define LIBRE 1
 #define SO *
 
+void insertar(int id,int tam_pro);
+void recorrer();
+void liberar(int id);
+void compactar();
+int rand_range(int min_n, int max_n);
+void darRequerimientos(int n);
+
+int cant_process;//CANTIDAD DE PROCEESOS
 int cant; //SERA EL IDENTIFICADOR DE LAS PARTICIONES
 //TODOS LOS TAMAÑOS DE LOS PROCESOS Y MEMORIA SERAN EN KB
 int main(){
@@ -25,20 +34,39 @@ int main(){
 	memoria->siguiente=NULL;
 	memoria->anterior=NULL;
 	memoria->ID=0;
-	memoria->pro='*';
+	memoria->pro=-1;
 	cant=1;
+	cant_process=0;
 	printf("MEMORIA DE :%i KB\n",memoria->size);
-	insertar('A',15360);
-	insertar('D',15360);
-	insertar('C',15360);
+	/*insertar(10,8192);
+	insertar(11,15360);
+	insertar(12,8192);
+	insertar(13,8192);
+	insertar(14,15360);
 	recorrer();
-	liberar('D');
-	insertar('X',8192);
+	liberar(11);
+	//recorrer();
+	liberar(13);
+	//recorrer();
+	insertar(15,8192);
+	*/
+	//darRequerimientos(15);
+	
+	insertar(0,608);
+	insertar(1,10037);
+	insertar(2,9543);
+	liberar(0);
+	insertar(3,3484);
+	liberar(3);
+	insertar(4,3861);
+	liberar(1);
+	recorrer();
+	insertar(5,2568);
 	recorrer();
 };
 
-void insertar(char id,int tam_pro){
-	printf("Estoy por insertar el Proceso: %c DE :%i KB\n",id,tam_pro);
+void insertar(int id,int tam_pro){
+	printf("\n----------------------- Insertando el Proceso: %i DE :%i KB ----------------------- \n",id,tam_pro);
 	struct celda* aux=(struct celda *)malloc(sizeof(struct celda));
 	struct celda* candidato=(struct celda *)malloc(sizeof(struct celda));
 	aux=memoria;
@@ -48,14 +76,13 @@ void insertar(char id,int tam_pro){
 	//BUSCO EL MEJOR CANDIDATO
 	while(aux!=NULL){
 		tam_particion=aux->size;
-		if((aux->estado=LIBRE)&&(tam_particion>=tam_pro)&&(tam_particion<=menor)){
+		if((aux->estado==LIBRE)&&(tam_particion>=tam_pro)&&(tam_particion<=menor)){
 			candidato=aux;
 			menor=tam_particion;
 		}
 		aux=(aux->siguiente);
 	};
-	if(menor>=tam_pro){
-		printf("--- Se aloco exitosamente el proceso: %c ---\n",id);
+	if(menor>tam_pro){
 		struct celda* nueva=(struct celda *)malloc(sizeof(struct celda));
 		nueva->ID=cant;
 		nueva->estado=OCUPADO;
@@ -68,50 +95,123 @@ void insertar(char id,int tam_pro){
 			 //MEDIO CHANCHO, ESTO ES EN CASO DE SER EL PRIMER PROCESO EN USAR LA MEMORIA SE MODIFIQUE EL PUNTERO
 		}
 		else{
-			printf("CAMBIE EL PUNTERO\n");
 			memoria=nueva;
 		}
 
-		candidato->size=tam_particion-tam_pro;
-		candidato->anterior=nueva;
+		(candidato->size)=menor-tam_pro;
+		(candidato->anterior)=nueva;
 		cant++;
+		cant_process++;
+		printf("		--- Se aloco exitosamente el proceso: %i en la particion:%i ---\n",id,candidato->ID);
+	}
+	else if(menor==tam_pro){
+		candidato->pro=id;
+		candidato->estado=OCUPADO;
+		printf("		--- Se aloco exitosamente el proceso: %i en la particion:%i ---\n",id,candidato->ID);
 	}
 	else
-		printf("Imposible alocar ese proceso");
-}
+		printf("		--- Imposible alocar ese proceso ---\n");
+}	
 
 
 void recorrer(){
 	struct celda* aux=(struct celda *)malloc(sizeof(struct celda));
 	aux=memoria;
 	int suma=0;
-	printf("\n--- Recorrido de la memoria --- \n*Particiones:\n*Memoria:%p\n",memoria);
+	printf("\n----------------------- Recorrido de la memoria ----------------------- \n*Particiones:\n");
 	while(aux!=NULL){
-		printf("\nParticion: %i :\n*Estado (Ocupado=0, Libre=1): %i\n*Tamaño:%i KB\n*Proceso: %c\n",aux->ID,aux->estado,aux->size,aux->pro);
+		printf("\nParticion: %i :\n*Estado (Ocupado=0, Libre=1): %i\n*Tamaño:%i KB\n*Proceso: %i\n",aux->ID,aux->estado,aux->size,aux->pro);
 		if(aux->estado==LIBRE)
 			suma=suma+(aux->size);
 		aux=(aux->siguiente);
 	};
 	printf("\n*Fracmentacion externa:%i KB\n\n",suma);
 }
-
-void liberar(char id){
+void liberar(int id){
 	struct celda* aux=(struct celda *)malloc(sizeof(struct celda));
 	aux=memoria;
 	int encontre=0;
-	char proceso;
-	printf("---------- ESTOY POR BORRAR, MI DIRECCION DE MEMORIA ES:%p-----------------------\n",memoria);
-	while(encontre==0){
+	int proceso;
+	printf("\n----------------------- ELIMINANDO EL PROCESO:%i -----------------------\n",id);
+	while((encontre==0)&&(aux!=NULL)){
 		proceso=(aux->pro);
+		//printf("Id del proceso recorrido:%i \n",proceso);
 		if(proceso==id){
 			encontre=1;
 			aux->estado=LIBRE;
-			aux->pro='*';
-			printf("--- Se elimino exitosamente el Proceso: %c ---\n",id);
+			aux->pro=-1;
+			printf("		--- Se elimino exitosamente el Proceso: %i ---\n",id);
+			compactar();
+			//compactar();
 		}
 		else 
 			aux=(aux->siguiente);
 	}
 	if(encontre==0)
-		printf("--- No existe el Proceso: %c ---\n",id);
-}
+		printf("		--- No existe el Proceso: %i ---\n",id);
+};
+void compactar(){
+	struct celda* aux=(struct celda *)malloc(sizeof(struct celda));
+	struct celda* ant=(struct celda *)malloc(sizeof(struct celda));
+	aux=memoria;
+	int proceso;
+	printf("\n 			--- COMPACTANDO --- \n");
+	while(aux!=NULL){
+		ant=aux->anterior;
+		if(ant!=NULL){
+			if((aux->estado==LIBRE)&&(ant->estado==LIBRE)){
+				//TENGO QUE COMPACTAR DOS UNIDADES LIBRES
+				int size=ant->size;
+				ant->size=size+aux->size;
+				struct celda* sig=(struct celda *)malloc(sizeof(struct celda));
+				sig=aux->siguiente;
+				if(sig==NULL){
+					ant->siguiente=NULL;
+					free(aux);
+				}
+				else{
+					ant->siguiente=sig;
+					(sig->anterior)=ant;
+					free(aux);
+				};	
+			aux=ant;
+			}
+			else{
+				aux=aux->siguiente;
+			}
+		}
+		else{
+			aux=aux->siguiente;
+		}
+	};
+};
+
+
+int rand_range(int min_n, int max_n){
+    return rand() % (max_n - min_n + 1) + min_n;
+};
+
+
+void darRequerimientos(int n){
+	int cont=0;
+	int i=0;
+	time_t t;
+   	int size;
+   	int r;
+   //	srand((unsigned) time(&t));
+	printf("\n\n----------------------- DAR REQUERIMIENTOS ----------------------- \n");
+	while(i!=n){
+		r= rand() % 2;
+		if(r==0){
+			//ELIJO UN PROCESO AL AZAR PARA LIBERAR
+			int victima=rand()%cant_process;
+			liberar(victima);
+		}
+		else{
+			//CREO NUEVOS REQUERIMIENTOS
+			size=rand_range(100,16348);
+			insertar(cant_process,size);
+		}
+		i++;
+	};
+};
--- Proyecto2/ejercicioPrimerLugar.c
+++ Proyecto2/ejercicioPrimerLugar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct celda{
 	int estado;
@@ -15,6 +16,14 @@ struct celda{
 #define LIBRE 1
 #define SO *
 
+void insertar(int id,int tam_pro);
+void recorrer();
+void liberar(int id);
+void compactar();
+int rand_range(int min_n, int max_n);
+void darRequerimientos(int n);
+
+int cant_process;//CANTIDAD DE PROCEESOS
 int cant; //SERA EL IDENTIFICADOR DE LAS PARTICIONES
 //TODOS LOS TAMAÑOS DE LOS PROCESOS Y MEMORIA SERAN EN KB
 int main(){
@@ -27,17 +36,15 @@ int main(){
 	memoria->pro=-1;
 	cant=1;
 	printf("MEMORIA DE :%i KB\n",memoria->size);
-	insertar(1,15360);
-	insertar(2,15360);
-	insertar(3,15360);
+	int* procesos=malloc(sizeof(int*)*5);
+	cant_process=0;
 	recorrer();
-	liberar(4);
-	insertar(5,8192);
+	darRequerimientos(10);
 	recorrer();
 };
 
 void insertar(int id,int tam_pro){
-	printf("Estoy por insertar el Proceso: %i DE :%i KB\n",id,tam_pro);
+	printf("\n *************** Insertar el Proceso: %i DE :%i KB *************** \n",id,tam_pro);
 	struct celda* aux=(struct celda *)malloc(sizeof(struct celda));
 	aux=memoria;
 	int entro=0;
@@ -57,7 +64,7 @@ void insertar(int id,int tam_pro){
 				 //MEDIO CHANCHO, ESTO ES EN CASO DE SER EL PRIMER PROCESO EN USAR LA MEMORIA SE MODIFIQUE EL PUNTERO
 			}
 			else{
-				printf("CAMBIE EL PUNTERO\n");
+				//printf("CAMBIE EL PUNTERO\n");
 				memoria=nueva;
 			}
 
@@ -65,12 +72,14 @@ void insertar(int id,int tam_pro){
 			aux->anterior=nueva;
 			entro=1;
 			cant++;
+			cant_process++;
+			printf(" 	--- El proceso fue alocado exitosamente ---\n");
 		}
 		else 
 			aux=(aux->siguiente);
 	}
 	if(entro==0)
-		printf("Imposible alocar ese proceso");
+		printf(" 	---Imposible alocar ese proceso ---\n");
 }
 
 
@@ -78,7 +87,7 @@ void recorrer(){
 	struct celda* aux=(struct celda *)malloc(sizeof(struct celda));
 	aux=memoria;
 	int suma=0;
-	printf("\n--- Recorrido de la memoria --- \n*Particiones:\n*Memoria:%p\n",memoria);
+	printf("\n *************** Recorrido de la memoria *************** \n*Particiones:\n*Memoria:%p \n",memoria);
 	while(aux!=NULL){
 		printf("\nParticion: %i :\n*Estado (Ocupado=0, Libre=1): %i\n*Tamaño:%i KB\n*Proceso: %i\n",aux->ID,aux->estado,aux->size,aux->pro);
 		if(aux->estado==LIBRE)
@@ -93,19 +102,87 @@ void liberar(int id){
 	aux=memoria;
 	int encontre=0;
 	int proceso;
-	printf("---------- ESTOY POR BORRAR, MI DIRECCION DE MEMORIA ES:-----------------------\n",memoria);
+	printf("\n *************** ELIMANDO EL PROCESO:%i *************** \n",id);
 	while((encontre==0)&&(aux!=NULL)){
 		proceso=(aux->pro);
-		printf("Id del proceso recorrido:%i \n",proceso);
+		//printf("Id del proceso recorrido:%i \n",proceso);
 		if(proceso==id){
 			encontre=1;
 			aux->estado=LIBRE;
 			aux->pro=-1;
-			printf("--- Se elimino exitosamente el Proceso: %i ---\n",id);
+			printf("	  --- Se elimino exitosamente el Proceso: %i ---\n",id);
+			compactar();
+			//compactar();
 		}
 		else 
 			aux=(aux->siguiente);
 	}
 	if(encontre==0)
-		printf("--- No existe el Proceso: %i ---\n",id);
+		printf("		  --- No existe el Proceso: %i ---\n",id);
 }
+
+void compactar(){
+	struct celda* aux=(struct celda *)malloc(sizeof(struct celda));
+	struct celda* ant=(struct celda *)malloc(sizeof(struct celda));
+	aux=memoria;
+	int proceso;
+	while(aux!=NULL){
+		ant=aux->anterior;
+		if(ant!=NULL){
+			if((aux->estado==LIBRE)&&(ant->estado==LIBRE)){
+				//TENGO QUE COMPACTAR DOS UNIDADES LIBRES
+				int size=ant->size;
+				ant->size=size+aux->size;
+				struct celda* sig=(struct celda *)malloc(sizeof(struct celda));
+				sig=aux->siguiente;
+				if(sig==NULL){
+					ant->siguiente=NULL;
+					free(aux);
+				}
+				else{
+
+					ant->siguiente=sig;
+					(sig->anterior)=ant;
+					free(aux);
+				};	
+			aux=ant;
+			}
+			else{
+				aux=aux->siguiente;
+			}
+		}
+		else{
+			aux=aux->siguiente;
+		}
+	};
+};
+
+
+int rand_range(int min_n, int max_n){
+    return rand() % (max_n - min_n + 1) + min_n;
+}
+
+
+void darRequerimientos(int n){
+	int cont=0;
+	int i=0;
+	time_t t;
+   	int size;
+   	int r;
+   //	srand((unsigned) time(&t));
+	printf("\n ----------------------- DAR REQUERIMIENTOS ----------------------- \n");
+	while(i!=n){
+		r= rand() % 2;
+		if(r==0){
+			//ELIJO UN PROCESO AL AZAR PARA LIBERAR
+			int victima=rand()%cant_process;
+			liberar(victima);
+		}
+		else{
+			//CREO NUEVOS REQUERIMIENTOS
+			size=rand_range(100,16348);
+			insertar(cant_process,size);
+		}
+		i++;
+	};
+};
